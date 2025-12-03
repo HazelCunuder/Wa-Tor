f7d34e47880c5ced16691fb6ce36015aee95118d
Hazel <hazel.claire.lee@gmail.com>
2025-12-03 14:55:40 +0100

    feat: add display_grid method

--- world.py
+++ world.py
@@ -10,3 +10,19 @@ class World:
     def init_grid(self) -> list[list[str]]:
         # We use _ here because we won't use this variable for anything else in the entire code
         return [[" " for _ in range(self.grid_width)] for _ in range(self.grid_height)]
+    
+    def display_grid(self) -> list[list[str]]:
+        display = self.init_grid()
+        
+        for x in range(len(self.grid)):
+            for y in range(len(self.grid[0])):
+                cell = self.grid[y][x]
+                if isinstance(cell, Shark):
+                    display[y][x] = cell.emoji_shark
+                elif isinstance(cell, Tuna):
+                    display[y][x] = cell.emoji_fish
+                else:
+                    display[y][x] = " "
+        return display
+    
+    
