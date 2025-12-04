8780ffce6d7dc5b65f5c595ad156f8d91e835c72
alexandre <alexandre.crestien@gmail.com>
2025-12-04 10:13:59 +0100

    refactor: improve type hints in all class

--- src/entities/fish.py
+++ src/entities/fish.py
@@ -2,15 +2,22 @@ from networkx import neighbors
 
 
 class Fish:
-    def __init__(self, pos_x , pos_y):
+    def __init__(self, pos_x: int , pos_y: int)-> None:
         self.pos_x: int = pos_x
         self.pos_y: int = pos_y
 
     def move(self, grid):
         move: list = self.get_available_spaces(grid)
+
         print(move)
         pass
 
+    def choose_move(self, available_moves):
+        if not available_moves:
+            return None
+        import random
+        return random.choice(available_moves)
+    
     def  get_available_spaces(self, grid):
         height:  int = len(grid)
         width:  int = len(grid[0])
@@ -30,3 +37,12 @@ class Fish:
 
     def reproduce(self):
         pass
+
+fish = Fish(1, 1)
+grid = [
+    [None, None, None],
+    [None, None, None],
+    [None, None, None]
+]
+
+fish.move(grid)
--- src/entities/tuna.py
+++ src/entities/tuna.py
@@ -1,7 +1,7 @@
 from fish import Fish
 
 class Tuna (Fish):
-    def __init__(self, pos_x, pos_y, emoji_tuna="🐟"):
+    def __init__(self, pos_x: int, pos_y: int)-> None:
         super().__init__(pos_x, pos_y)
-        self.emoji_tuna : str = emoji_tuna
         self.reproduction_time: int = 3
+        self.emoji_tuna: str ="🐟"
