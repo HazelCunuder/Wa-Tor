0d932010bec13e33f8bfb5c73231aff89a2b686c
alexandre <alexandre.crestien@gmail.com>
2025-12-04 15:14:06 +0100

    feat: finalize shark movement and tuna targeting logic

--- src/entities/shark.py
+++ src/entities/shark.py
@@ -1,6 +1,7 @@
+from __future__ import annotations
 from .fish import Fish
 from .tuna import Tuna
-from __future__ import annotations
+import random
 
 class Shark(Fish):
     def __init__(self, pos_x: int, pos_y: int)-> None:
@@ -8,19 +9,33 @@ class Shark(Fish):
         self.energy: int = 5
         self.reproduction_time: int = 5
         self.emoji_shark: str ="🦈"
+        self.is_alive: bool = True
+    
+    def choose_move(self, available_moves: list[tuple[int, int]], grid: list[list[Fish | None]]) -> tuple[int, int]:
+        if not available_moves:
+            return super().choose_move(available_moves)
+        
+        if isinstance(grid[available_moves[0][1]][available_moves[0][0]], Tuna):
+            is_fish = random.choice(available_moves)
+            grid[is_fish[1]][is_fish[0]].is_alive = False
+            self.energy += 2
+            return is_fish
+
+        return super().choose_move(available_moves)
     
     def get_available_spaces(self, grid: list[list[Fish | None]]) -> list[tuple[int, int]]:
         available:  list[tuple[int, int]] = []
         neighbors:  list[tuple[int, int]] = self.get_neighbors(grid)
         
         for (x, y) in neighbors:
-            if  isinstance(grid[y][x], Tuna):
+            if  isinstance(grid[y][x], Tuna) and grid[y][x].is_alive:
                 available.append((x, y))
         if available:
-            self.energy += 2
             return available
         else:
             self.energy -= 1
+            if self.energy <= 0:
+                self.is_alive = False
             return super().get_available_spaces(grid)
         
     def reproduce(self, pos_x: int, pos_y: int) -> Shark | None:
--- src/entities/tuna.py
+++ src/entities/tuna.py
@@ -1,5 +1,5 @@
-from .fish import Fish
 from __future__ import annotations
+from .fish import Fish
 
 class Tuna (Fish):
     def __init__(self, pos_x: int, pos_y: int)-> None:
@@ -7,7 +7,7 @@ class Tuna (Fish):
         self.reproduction_time: int = 3
         self.emoji_tuna: str ="🐟"
         self.is_alive: bool = True
-        
+
     def reproduce(self, pos_x: int, pos_y: int) -> Tuna | None:
         if self.reproduction_time <= 0:
             self.reproduction_time = 3
