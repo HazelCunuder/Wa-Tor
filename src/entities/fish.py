76a09dbb0cf67fc1b94386b50249fd887e29fc2c
alexandre <alexandre.crestien@gmail.com>
2025-12-04 09:18:36 +0100

    refactor: improve type hints in Fish class

--- src/entities/fish.py
+++ src/entities/fish.py
@@ -1,6 +1,6 @@
 class Fish:
     def __init__(self, pos_x, pos_y, reproduction_time=0):
-        self.pos_x = pos_x
+        self.pos_x: int = pos_x
         self.pos_y = pos_y
         self.reproduction_time = reproduction_time
 
--- src/entities/tuna.py
+++ src/entities/tuna.py
@@ -1,6 +1,6 @@
 from fish import Fish
 
 class Tuna (Fish):
-    def __init__(self, pos_x, pos_y, reproduction_time= 0, emoji="🐟"):
+    def __init__(self, pos_x, pos_y, reproduction_time= 0, emoji_tuna="🐟"):
         super().__init__(pos_x, pos_y, reproduction_time)
-        self.emoji = emoji
+        self.emoji_tuna : str = emoji_tuna
