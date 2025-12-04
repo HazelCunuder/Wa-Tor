65c4276df3c7f778cdb11b802c528db9dc18c261
Hazel <hazel.claire.lee@gmail.com>
2025-12-04 14:37:27 +0100

    feat: add main.py

---
+++ src/main.py
@@ -0,0 +1,23 @@
+from world import World
+from entities import *
+
+input_height: int = int(input("Enter a height: "))
+input_width: int = int(input("Enter a width: "))
+input_tunas: int = int(input("How many tunas in the simulation? "))
+input_max_chronons: int = int(input("How many chronons long should the experiment be? "))
+
+planet: World = World(height= input_height, width=input_width)
+
+planet.randomly_place_fishes(nb_sharks = 0, nb_tunas = input_tunas)
+
+step: int = 1
+
+limit: int = input_max_chronons
+
+while planet.chronons < limit:
+    planet.run_simulation()
+    print("\n")
+    print(f"Step {step}:")
+    step += 1
+    planet.print_grid_ascii()
+    
