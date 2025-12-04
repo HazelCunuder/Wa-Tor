1d2cdbaebaefd3ab9a4f907cd7d4c2be9a69462a
alexandre <alexandre.crestien@gmail.com>
2025-12-04 12:08:29 +0100

    Refactor Fish class: add proper typing

--- src/entities/fish.py
+++ src/entities/fish.py
@@ -1,5 +1,5 @@
-from networkx import neighbors
 import random
+from __future__ import annotations
 
 class Fish:
     def __init__(self, pos_x: int , pos_y: int)-> None:
@@ -9,23 +9,23 @@ class Fish:
         self.is_alive: bool = True
         self.reproduction_time: int = 3
 
-    def move(self, grid: list) -> tuple:
-        available_moves : list = self.get_available_spaces(grid)
-        move: tuple = self.choose_move(available_moves)
+    def move(self, grid:  list[list]) -> tuple[int, int]:
+        available_moves : list[tuple[int, int]] = self.get_available_spaces(grid)
+        move: tuple[int, int] = self.choose_move(available_moves)
         self.pos_x, self.pos_y = move
         return move
 
-    def choose_move(self, available_moves: list) -> tuple:
+    def choose_move(self, available_moves: list[tuple[int, int]]) -> tuple[int, int]:
         if not available_moves:
             return (self.pos_x, self.pos_y)
         return random.choice(available_moves)
     
-    def  get_available_spaces(self, grid: list) -> list:
+    def  get_available_spaces(self, grid: list[list]) -> list[tuple[int, int]]:
         height:  int = len(grid)
         width:  int = len(grid[0])
-        available:  list = []
+        available:  list[tuple[int, int]] = []
 
-        neighbors:  list = [
+        neighbors:  list[tuple[int, int]] = [
             ((self.pos_x + 1) % width, self.pos_y),
             ((self.pos_x - 1) % width, self.pos_y),
             (self.pos_x, (self.pos_y + 1) % height),
@@ -33,11 +33,11 @@ class Fish:
         ]
 
         for (x, y) in neighbors:
-            if grid[y][x] is " ":
+            if grid[y][x] == " ":
                 available.append((x, y))
         return available
 
-    def reproduce(self, pos_x: int, pos_y: int) -> object | None:
+    def reproduce(self, pos_x: int, pos_y: int) -> Fish | None:
         if self.reproduction_time <= 0:
             self.reproduction_time = 3
             return Fish(pos_x, pos_y)
