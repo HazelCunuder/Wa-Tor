f0fe44800a56c36fdd136714c6f1ca94f441daf7
Hazel <hazel.claire.lee@gmail.com>
2025-12-05 16:50:56 +0100

    feat: remove methods linked to simulation

--- src/world.py
+++ src/world.py
@@ -7,6 +7,7 @@ class World:
     def __init__(self, height: int, width: int) -> None:
         self.grid_height: int = height
         self.grid_width: int = width
+        self.grid_size: int = height * width
         self.grid = self.init_grid()
         self.chronons: int = 0
         self.fishes: list[Fish] = []
@@ -17,26 +18,13 @@ class World:
         # We use _ here because we won't use this variable for anything else in the entire code
         return [[None for _ in range(self.grid_width)] for _ in range(self.grid_height)]
     
-    def display_grid(self) -> list[list[str]]:
-        display = [[" " for _ in range(self.grid_width)] for _ in range(self.grid_height)]
-        
-        for y in range(len(self.grid)):
-            for x in range(len(self.grid[0])):
-                cell = self.grid[y][x]
-                if isinstance(cell, Shark):
-                    display[y][x] = cell.emoji_shark
-                elif isinstance(cell, Tuna):
-                    display[y][x] = cell.emoji_tuna
-                else:
-                    display[y][x] = " "
-        return display
-    
     def new_tuna(self, tuna: Tuna):
         if not self.is_position_valid(x=tuna.pos_x, y=tuna.pos_y):
             return None
         if len(self.tunas) >= (self.grid_width * self.grid_height):
             return None
         self.tunas.append(tuna)
+        self.fishes.append(tuna)
         self.grid[tuna.pos_y][tuna.pos_x] = tuna
         
     def new_shark(self, shark: Shark):
@@ -45,6 +33,7 @@ class World:
         if len(self.tunas) >= (self.grid_width * self.grid_height):
             return None
         self.sharks.append(shark)
+        self.fishes.append(shark)
         self.grid[shark.pos_y][shark.pos_x] = shark
         
     def is_position_valid(self, x: int, y: int) -> bool:
@@ -78,58 +67,4 @@ class World:
                     self.new_tuna(tuna)
                     break
     
-    def run_simulation(self):
-        new_sharks: list[Shark] = []
-        new_tunas: list[Tuna] = []
-        
-        for shark in self.sharks:
-            if shark and shark.is_alive:
-                old_x = shark.pos_x
-                old_y = shark.pos_y
-                new_pos = shark.move(self.grid)
-                self.grid[old_y][old_x] = None
-                self.grid[new_pos[1]][new_pos[0]] = shark
-                baby_shark = shark.reproduce(pos_x= old_x, pos_y= old_y)
-                if baby_shark and self.is_position_valid(x= baby_shark.pos_x, y= baby_shark.pos_y):
-                    self.grid[baby_shark.pos_y][baby_shark.pos_x] = baby_shark
-                    new_sharks.append(baby_shark)
-            if shark and not shark.is_alive:
-                old_x = shark.pos_x
-                old_y = shark.pos_y
-                self.grid[old_y][old_x] = None
-                self.sharks.remove(shark)
 
-        for tuna in self.tunas:
-            if tuna and tuna.is_alive:
-                old_x = tuna.pos_x
-                old_y = tuna.pos_y
-                new_pos = tuna.move(self.grid)
-                self.grid[old_y][old_x] = None
-                self.grid[new_pos[1]][new_pos[0]] = tuna
-                baby_tuna = tuna.reproduce(pos_x = old_x, pos_y = old_y)
-                if baby_tuna and self.is_position_valid(x = baby_tuna.pos_x, y = baby_tuna.pos_y):
-                    self.grid[baby_tuna.pos_y][baby_tuna.pos_x] = baby_tuna
-                    new_tunas.append(baby_tuna)
-            if tuna and not tuna.is_alive:
-                old_x = tuna.pos_x
-                old_y = tuna.pos_y
-                self.grid[old_y][old_x] = None
-                self.tunas.remove(tuna)
-      
-        self.sharks.extend(new_sharks)
-        self.tunas.extend(new_tunas)
-        self.chronons += 1
-        
-    def print_grid_ascii(self):
-        visual = self.display_grid()
-      
-        # Top border
-        print("+" + ("---+" * len(visual[0])))
-      
-        # Rows
-        for row in visual:
-            print("|", end="")
-            for cell in row:
-                print(f" {cell} |", end="")
-            print()
-            print("+" + ("---+" * len(visual[0])))
