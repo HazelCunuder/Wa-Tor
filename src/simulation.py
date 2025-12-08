c6f7d34caeeb8354e38b0efdb0d5eb3c4bce00fc
Hazel <hazel.claire.lee@gmail.com>
2025-12-08 10:01:57 +0100

    feat: revamped the run_simulation method to call the world cycle from world directly, as well as handling the loss conditions

--- src/simulation.py
+++ src/simulation.py
@@ -20,54 +20,18 @@ class Simulation:
                     display[y][x] = " "
         return display
     
-    def run_simulation(self):
-        new_sharks: list[Shark] = []
-        new_tunas: list[Tuna] = []    
-    
-    
-        for shark in self.world.sharks:
-            if shark and shark.is_alive:
-                old_x = shark.pos_x
-                old_y = shark.pos_y
-                new_pos = shark.move(self.world.grid)
-                self.world.grid[old_y][old_x] = None
-                self.world.grid[new_pos[1]][new_pos[0]] = shark
-                baby_shark = shark.reproduce(pos_x= old_x, pos_y= old_y)
-                if baby_shark and self.world.is_position_valid(x= baby_shark.pos_x, y= baby_shark.pos_y):
-                    self.world.grid[baby_shark.pos_y][baby_shark.pos_x] = baby_shark
-                    new_sharks.append(baby_shark)
-            if shark and not shark.is_alive:
-                old_x = shark.pos_x
-                old_y = shark.pos_y
-                self.world.grid[old_y][old_x] = None
-                self.world.sharks.remove(shark)
-                self.world.fishes.remove(shark)
-
-        for tuna in self.world.tunas:
-            if tuna and tuna.is_alive:
-                old_x = tuna.pos_x
-                old_y = tuna.pos_y
-                new_pos = tuna.move(self.world.grid)
-                self.world.grid[old_y][old_x] = None
-                self.world.grid[new_pos[1]][new_pos[0]] = tuna
-                baby_tuna = tuna.reproduce(pos_x = old_x, pos_y = old_y)
-                if baby_tuna and self.world.is_position_valid(x = baby_tuna.pos_x, y = baby_tuna.pos_y):
-                    self.world.grid[baby_tuna.pos_y][baby_tuna.pos_x] = baby_tuna
-                    new_tunas.append(baby_tuna)
-            if tuna and not tuna.is_alive:
-                old_x = tuna.pos_x
-                old_y = tuna.pos_y
-                self.world.grid[old_y][old_x] = None
-                self.world.tunas.remove(tuna)
-                self.world.fishes.remove(tuna)
-
-        self.world.sharks.extend(new_sharks)
-        self.world.tunas.extend(new_tunas)
+    def run_simulation(self, world: World):
+        print("Initial state: ")
+        self.print_grid_ascii()
         
-        self.world.fishes.extend(new_sharks)
-        self.world.fishes.extend(new_tunas)
-        
-        self.world.chronons += 1
+        while not self.is_simulation_over():
+            print("\n")
+            world.world_cycle()
+            self.print_grid_ascii()
+            
+        print(f"Number of sharks left: {len(world.sharks)}")
+        print(f"Number of tunas left: {len(world.tunas)}")
+        print(f"Total Chronons: {world.chronons}")
         
     def print_grid_ascii(self):
         visual = self.display_grid()
