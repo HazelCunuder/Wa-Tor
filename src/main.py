d430e7d2d6d83687bea482c7b6998253047c0547
Hazel <hazel.claire.lee@gmail.com>
2025-12-10 12:22:06 +0100

    feat: temporarily add megalodon argument

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ config = ConfigurationWator()
 
 planet: World = World(config)
 
-planet.randomly_place_fishes(nb_sharks = config.nb_shark, nb_tunas = config.nb_tuna)
+planet.randomly_place_fishes(nb_sharks = config.nb_shark, nb_tunas = config.nb_tuna, nb_megalodons=1)
 
 sim = Simulation(planet)
 
