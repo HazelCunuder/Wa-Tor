c6ae7126d982396c9215bc452a03dd02f0d3b765
alexandre <alexandre.crestien@gmail.com>
2025-12-11 04:07:45 +0100

    feat: add dynamic canvas resizing for simulation grid

--- src/interface/simulation_page.py
+++ src/interface/simulation_page.py
@@ -4,22 +4,28 @@ from entities.shark import Shark
 from entities.megalodon import Megalodon
 
 class SimulationPage(tk.Frame):
-
-
     def __init__(self, parent, controller):
         super().__init__(parent)
         self.controller = controller
         self.cell_size = 10
         self.is_running = False
 
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
         self.canvas = tk.Canvas(self, bg="black")
-        self.canvas.pack(fill="both", expand=True)
-        self.next_button = tk.Button(self, text="Next step", command = self.next_step)
-        self.next_button.pack(pady=10)
-        self.reset_button = tk.Button(self, text="Reset", command = self.reset_simulation)
-        self.reset_button.pack(pady=10)
-        self.run_simulation_button = tk.Button(self, text="Start", command = self.run_simulation)
+        self.canvas.grid(row=0, column=0, sticky="nsew")
+        self.canvas.bind("<Configure>", self.resize_simulation)
+        
+        widget_frame = tk.Frame(self)
+        widget_frame.grid(row=0, column=1, padx=50)
+
+        self.next_button = tk.Button(widget_frame, text="Next step", command=self.next_step)
+        self.next_button.pack(pady=5)
+        self.run_simulation_button = tk.Button(widget_frame, text="Start", command=self.run_simulation)
         self.run_simulation_button.pack(pady=5)
+        self.reset_button = tk.Button(widget_frame, text="Reset", command=self.reset_simulation)
+        self.reset_button.pack(pady=5)
         self.draw_grid()
 
     def draw_grid(self):
@@ -27,12 +33,12 @@ class SimulationPage(tk.Frame):
         world = self.controller.world
 
         for y in range(world.grid_height):
-            for x in range(world.grid_width):
+            for x in range( world.grid_width):
 
-                x1 = x * self.cell_size
-                y1 = y * self.cell_size
-                x2 = x1 + self.cell_size
-                y2 = y1 + self.cell_size
+                x1 = x * (self.canvas.winfo_width() / world.grid_width)
+                x2 = x1 + (self.canvas.winfo_width() / world.grid_width)
+                y1 = y * (self.canvas.winfo_height() / world.grid_height)
+                y2 = y1 + (self.canvas.winfo_height() / world.grid_height)
 
                 entity = world.grid[y][x]
 
@@ -75,3 +81,8 @@ class SimulationPage(tk.Frame):
         self.controller.world.world_cycle()
         self.draw_grid()
         self.after(800, self.auto_run)
+
+    def resize_simulation(self, event):
+        self.canvas_width = event.width
+        self.canvas_height = event.height
+        self.draw_grid()
